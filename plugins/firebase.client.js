95807ac53861822671efb36b2134e51c7892d61c
Dorkside <jdmartin@gmx.fr>
2023-03-16 10:31:10 +0100

    fix: logout and reload

--- plugins/firebase.client.js
+++ plugins/firebase.client.js
@@ -17,7 +17,7 @@ const config = {
   appId: "1:1056062064150:web:94e282256a1b11dac71737",
 };
 
-export default defineNuxtPlugin(({$pinia}) => {
+export default defineNuxtPlugin(({$pinia, ...other}) => {
   const app = initializeApp(config);
   const auth = getAuth(app);
   const firestore = getFirestore(app);
@@ -28,7 +28,10 @@ export default defineNuxtPlugin(({$pinia}) => {
     if (user) {
       userStore.setUser(user);
     } else {
-      userStore.setUser(null);
+      if (userStore.user) {
+        userStore.setUser(null);
+        window.location.reload(true);
+      }
     }
   });
 
